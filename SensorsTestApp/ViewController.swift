b972d200937fc38e1d0afee9610864787a3b88b8
Maksim Kalik <maxkalik@gmail.com>
2020-12-01 22:44:55 +0200

    Initial Commit

---
+++ SensorsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SensorsTestApp
+//
+//  Created by Maksim Kalik on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
